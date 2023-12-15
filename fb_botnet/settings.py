3ecc66a6b230db7f97d1c1c4d151675f4d4dadf3
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-15 16:45:49 +0700

    edit

--- fb_botnet/settings.py
+++ fb_botnet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s&fb@b62fo7m2#99-oawy%p1e%zyepbu7hj_h+)497jg*1ax)d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '127.0.0.1', '107.189.164.210']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -132,6 +132,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    '*',
+    'http://127.0.0.1:8000',
+    'http://0.0.0.0:8000',
+    'http://107.189.164.210:8000',
 ]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
