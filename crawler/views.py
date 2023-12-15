69bb1e6578137e3e21d0553095e03c98192423d7
namng <nam.nn@bidgear.com>
2023-12-16 00:10:29 +0700

    edit

--- crawler/views.py
+++ crawler/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import requests
 
 # Create your views here.
 def index(request):
@@ -15,23 +15,23 @@ def get_client_ip(request):
     return ip
 
 
-def get_geolocation(request):
+def get_geolocation(public_ip):
     try:
-        # Get the public IP address of the client
-        client_ip = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
-
         # Make a request to ipinfo.io to retrieve geolocation data
-        response = requests.get(f"https://ipinfo.io/{client_ip}/json")
+        response = requests.get(f"https://ipinfo.io/{public_ip}/json")
         geolocation_data = response.json()
 
         # Extract country code and city from the response
         country_code = geolocation_data.get("country")
         city = geolocation_data.get("city")
+        latitude, longitude = geolocation_data.get("loc").split(",")
 
         # Create a JSON response with the geolocation information
         response_data = {
             "country_code": country_code,
-            "city": city
+            "city": city,
+            "latitude": latitude,
+            "longitude": longitude
         }
         return response_data
     except Exception as e:
@@ -40,8 +40,12 @@ def get_geolocation(request):
 
 def process(request):
     if request.method == 'POST':
-        ip = get_client_ip(request)
-        print(ip)
+        final_data = {}
+        public_ip = get_client_ip(request)
+        geolocation = get_geolocation(public_ip)
+        final_data['public_ip'] = public_ip
+        final_data.update(geolocation)
+        print(final_data)
         return render(request=request, template_name='form.html')
     else:
         return render(request=request, template_name='form.html')
