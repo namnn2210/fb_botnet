5b418b7591f56013365f918f4776ff282849ee16
namng <nam.nn@bidgear.com>
2023-12-16 00:07:13 +0700

    edit

--- crawler/views.py
+++ crawler/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 def index(request):
     return render(request=request, template_name='index.html')
 
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -12,6 +13,30 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+
+def get_geolocation(request):
+    try:
+        # Get the public IP address of the client
+        client_ip = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+
+        # Make a request to ipinfo.io to retrieve geolocation data
+        response = requests.get(f"https://ipinfo.io/{client_ip}/json")
+        geolocation_data = response.json()
+
+        # Extract country code and city from the response
+        country_code = geolocation_data.get("country")
+        city = geolocation_data.get("city")
+
+        # Create a JSON response with the geolocation information
+        response_data = {
+            "country_code": country_code,
+            "city": city
+        }
+        return response_data
+    except Exception as e:
+        return None
+
 
 def process(request):
     if request.method == 'POST':
