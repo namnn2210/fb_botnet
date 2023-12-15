09364d2f715b7c133c89849c4d0eb0a8b4dd63c3
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-15 16:14:04 +0700

    edit

--- fb_botnet/settings.py
+++ fb_botnet/settings.py
@@ -126,11 +126,11 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'http://107.189.164.210:8000/',
+    'http://107.189.164.210:8000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'http://107.189.164.210:8000/',
+    'http://107.189.164.210:8000',
 ]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
