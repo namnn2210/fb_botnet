2f4daf93e176ddc44b2d6f820ab79d28c6363c08
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-15 16:41:44 +0700

    edit

--- crawler/views.py
+++ crawler/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(template_name='index.html')
+    return render(request=request, template_name='index.html')
 
 
 def process(request):
     if request.method == 'POST':
         print(request)
     else:
-        return render(template_name='form.html')
+        return render(request=request, template_name='form.html')
