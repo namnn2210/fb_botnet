84d14e53149c213ce11fa693ab4620711cb9c9be
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-15 16:40:58 +0700

    edit

--- crawler/views.py
+++ crawler/views.py
@@ -3,12 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request=request, template_name='index.html')
+    return render(template_name='index.html')
 
 
 def process(request):
     if request.method == 'POST':
-        client_ip = request.META.get('REMOTE_ADDR')
-        print(client_ip)
+        print(request)
     else:
-        return render(request=request, template_name='form.html')
+        return render(template_name='form.html')
