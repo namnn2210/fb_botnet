3b2a030bc402e184169135c30f35903d1ac3c20e
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-15 16:52:55 +0700

    edit

--- fb_botnet/settings.py
+++ fb_botnet/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-s&fb@b62fo7m2#99-oawy%p1e%zyepbu7hj_h+)497jg*1ax)d
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
@@ -123,17 +124,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'http://0.0.0.0:8000',
     'http://107.189.164.210:8000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'http://0.0.0.0:8000',
     'http://107.189.164.210:8000',
 ]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
